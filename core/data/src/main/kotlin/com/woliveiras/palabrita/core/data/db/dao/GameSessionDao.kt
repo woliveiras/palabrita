4f96a29051eda360bc39da648570a989fbcf3445
William Oliveira <w.oliveira542@gmail.com>
2026-04-21 19:48:41 +0200

    feat: Update insert method in GameSessionDao to handle conflicts with REPLACE strategy

--- core/data/src/main/kotlin/com/woliveiras/palabrita/core/data/db/dao/GameSessionDao.kt
+++ core/data/src/main/kotlin/com/woliveiras/palabrita/core/data/db/dao/GameSessionDao.kt
@@ -2,6 +2,7 @@ package com.woliveiras.palabrita.core.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.woliveiras.palabrita.core.data.db.entity.GameSessionEntity
@@ -9,7 +10,8 @@ import com.woliveiras.palabrita.core.data.db.entity.GameSessionEntity
 @Dao
 interface GameSessionDao {
 
-  @Insert suspend fun insert(session: GameSessionEntity): Long
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  suspend fun insert(session: GameSessionEntity): Long
 
   @Update suspend fun update(session: GameSessionEntity)
 
